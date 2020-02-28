9900a8fa84584518ff1567aec692748d94996e70
Zi Ye <18612319597@163.com>
2020-02-28 19:02:08 +0800

    create, update, complete, and delete

--- db/models/todo.js
+++ db/models/todo.js
@@ -3,8 +3,14 @@ module.exports = (sequelize, DataTypes) => {
   const Todo = sequelize.define('Todo', {
     name: DataTypes.STRING,
     deadline: DataTypes.DATE,
-    content: DataTypes.STRING
-  }, {});
+    content: DataTypes.STRING,
+    status: {
+      type: DataTypes.INTEGER,
+      defaultValue: 1
+    }
+  }, {
+    timestamps: false
+  });
   Todo.associate = function(models) {
     // associations can be defined here
   };
--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const models = require('../db/models');
 
 const app = express();
 
@@ -7,42 +8,81 @@ app.use(express.json());
 app.use(express.urlencoded());
 app.use(bodyParser.json());
 
-app.get('/list/:status/:page', (req, res) => {
-    res.json({
-        list: []
-    });
+app.get('/list/:status/:page', async (req, res, next) => {
+    // status: 1 -> 代办 2 -> 完成 3 -> 删除 -1 -> all
+    try {
+        const { status, page } = req.params;
+        const limit = 10;
+        const offset = (page - 1) * limit;
+        const where = status === '-1' ? {} : { status };
+        const list = await models.Todo.findAndCountAll({
+            where,
+            limit,
+            offset
+        });
+        res.json({
+            list,
+            message: '列表查询成功'
+        });
+    } catch(err) {
+        next(err);
+    }
 });
 
-app.post('/create', (req, res) => {
-    const { name, deadline, content } = req.body;
-    res.json({
-        todo: {
+app.post('/create', async (req, res, next) => {
+    try {
+        const { name, deadline, content } = req.body;
+        const todo = await models.Todo.create({
             name,
             deadline,
             content
-        }
-    });
+        });
+        res.json(todo);
+    } catch(err) {
+        next(err);
+    }
 });
 
-app.post('/update', (req, res) => {
-    const { name, deadline, content, id } = req.body;
-    res.json({
-        todo: {
-            id,
-            name,
-            deadline,
-            content
+app.post('/update', async (req, res, next) => {
+    try {
+        const { name, deadline, content, id } = req.body;
+        let todo = await models.Todo.findOne({
+            where: { id }
+        });
+        if(todo) {
+            todo = await todo.update({
+                name,
+                deadline,
+                content
+            });
+            res.json(todo);
+        } else {
+            res.status(500).end('todo isn\'t exist');
         }
-    });
+    } catch(err) {
+        next(err);
+    }
 });
 
-app.post('/update_status', (req, res) => {
-    const { id, status } = req.body;
-    res.json({
-        todo: {},
-        id,
-        status
-    });
+app.post('/update_status', async (req, res, next) => {
+    try {
+        const { id, status } = req.body;
+        let todo = await models.Todo.findOne({
+            where: { id }
+        });
+        if(todo) {
+            if(Number(status) !== todo.status) {
+                todo = await todo.update({ status });
+            }
+            res.json({
+                todo
+            });
+        } else {
+            res.status(500).end('todo isn\'t exist');
+        }
+    } catch(err) {
+        next(err);
+    }
 });
 
 app.use((err, req, res) => {
